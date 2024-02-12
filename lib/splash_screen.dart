e7e94c888f5adc28de07aa64f93cb2b5f58a4c79
fikrysetiaone <fikrysetiawan13@gmail.com>
2024-02-12 20:34:55 +0700

    Adding splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -7,19 +7,41 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      appBar: AppBar(
-        title: const Text("Learn Flutter Widgets"),
-        backgroundColor: Colors.amber,
-      ),
+      // appBar: AppBar(
+      //   title: const Text("Learn Flutter Widgets"),
+      //   backgroundColor: Colors.amber,
+      // ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             alignment: Alignment.center,
-            height: 100,
+            // height: 100,
             // color: Colors.amber,
-            child: const Text("Hi, Welcome To Flutter Learn"),
+            child: const Column(
+              children: [
+                Image(
+                  width: 400,
+                  image: AssetImage('images/welcome.png'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Hi, Welcome To Flutter Learn",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+              ],
+            ),
           ),
+
+          const SizedBox(height: 30),
+
+          // Start Button
+
           FilledButton(
             style: const ButtonStyle(
               backgroundColor: MaterialStatePropertyAll(Colors.black),
