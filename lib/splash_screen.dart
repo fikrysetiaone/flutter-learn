3879ce56f5f6ac4e8803fef1cfcd4fc48379bf8e
fikrysetiaone <fikrysetiawan13@gmail.com>
2024-02-12 19:57:38 +0700

    Add Text and start on splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -5,6 +5,27 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: const Text("Learn Flutter Widgets"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: 100,
+            // color: Colors.amber,
+            child: const Text("Hi, Welcome To Flutter Learn"),
+          ),
+          FilledButton(
+            onPressed: () {},
+            child: const Text("Press Me!"),
+          )
+        ],
+      ),
+    );
   }
 }
